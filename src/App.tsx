1779281c2e81b96d06e6d2c84cd3d8db4d8175d3
belalazam <belalazamtrue@gmail.com>
2024-03-02 19:18:46 +0530

    fixed code structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import LandingPage from './pages/LandingPage'
+import FooterEveryWhere from './components/FooterEveryWhere/FooterEveryWhere'
 import './App.css'
+import { Box } from '@mui/material'
 
 function App() {
   return (
-    <div>
+    <Box>
       <LandingPage />
-    </div>
+      <FooterEveryWhere />
+    </Box>
   )
 }
 
